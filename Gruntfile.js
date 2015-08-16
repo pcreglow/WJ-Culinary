acca028b02d432236b200f21fa8b21d4e65fedb4
Phillip Creglow <phillipcreglow@msn.com>
2015-08-16 14:23:23 -0400

    Added browser-sync to load tasks in grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -192,7 +192,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
+    grunt.loadNpmTasks('grunt-browser-sync');
     // Tasks
     grunt.registerTask('default', ['less']);
 };
